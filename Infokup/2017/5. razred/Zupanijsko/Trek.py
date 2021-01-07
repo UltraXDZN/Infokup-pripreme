9132d4f6cb058a138dd5f785ef9f6ca3314b651a
UltraXDZN <ultraxdzn@gmail.com>
2021-01-07 23:59:21 +0100

    Update trek

--- Infokup/2017/5. razred/Zupanijsko/Trek.py
+++ Infokup/2017/5. razred/Zupanijsko/Trek.py
@@ -1,6 +1,32 @@
 pocetni_broj = int(input())
 zavrsni_broj = int(input())
 
+
+def pronadji_najkraci_put(pocetni_broj, zavrsni_broj, graf):
+    posjeceni_brojevi = [pocetni_broj]
+    duljina_puta = []
+    for i in range(14):
+        if graf[pocetni_broj][i] != 0:
+            rekuzrzivna_duljina = pronadji_put(i, zavrsni_broj, graf, posjeceni_brojevi, graf[pocetni_broj][i])
+            if rekuzrzivna_duljina != -1:
+                duljina_puta.append(rekuzrzivna_duljina)
+
+    return min(duljina_puta)
+
+def pronadji_put(trenutni_broj, zavrsni_broj, posjeceni_brojevi, graf, pozicija_na_grafu):
+    if trenutni_broj == zavrsni_broj:
+        return pozicija_na_grafu
+
+    rekurzivne_duljine = []
+    for i in range(13):
+        if graf[trenutni_broj][i] != 0:
+            if i not in posjeceni_brojevi:
+                rekuzrzivna_duljina = pronadji_put(i, zavrsni_broj, graf, posjeceni_brojevi, graf[trenutni_broj][i] + pozicija_na_grafu)
+                rekurzivne_duljine.append(rekuzrzivna_duljina)
+    if ()
+    return min(rekurzivne_duljine)
+
+
 edgeovi = []
 for i in range(14):
     row = []
@@ -23,19 +49,16 @@ for i in range(7):
 
 print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in edgeovi]))
 
-brojac_prvi = []
-brojac_drugi = []
-for i in range(len(edgeovi)):
-    if (edgeovi[pocetni_broj][i] != 0):
-        brojac_prvi.append(edgeovi[pocetni_broj][i])
-
-
-for i in range(len(edgeovi)):
-    if(edgeovi[zavrsni_broj-1][i] != 0):
-        brojac_drugi.append(edgeovi[zavrsni_broj-1][i])
-
-# res = []
-# for i in range(14):
-#     if brojac[i] != 0:
-#         res.append(brojac[i])
-print(brojac_prvi[1] + brojac_drugi[1])
+print(pronadji_najkraci_put(pocetni_broj, zavrsni_broj, edgeovi))
+# brojac_prvi = []
+# brojac_drugi = []
+# for i in range(len(edgeovi)):
+#     if (edgeovi[pocetni_broj][i] != 0):
+#         brojac_prvi.append(edgeovi[pocetni_broj][i])
+#
+#
+# for i in range(len(edgeovi)):
+#     if(edgeovi[zavrsni_broj-1][i] != 0):
+#         brojac_drugi.append(edgeovi[zavrsni_broj-1][i])
+#
+# print(brojac_prvi, brojac_drugi)
