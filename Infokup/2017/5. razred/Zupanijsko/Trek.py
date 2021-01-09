6f297867d76d7d47042680567bb005d4bbe952e6
UltraXDZN <ultraxdzn@gmail.com>
2021-01-09 23:56:17 +0100

    Rjeseno - Zupanijsko 5. razred/2018.

--- Infokup/2017/5. razred/Zupanijsko/Trek.py
+++ Infokup/2017/5. razred/Zupanijsko/Trek.py
@@ -2,34 +2,39 @@ pocetni_broj = int(input())
 zavrsni_broj = int(input())
 
 
-def pronadji_najkraci_put(pocetni_broj, zavrsni_broj, graf):
-    posjeceni_brojevi = [pocetni_broj]
-    duljina_puta = []
+def pronadji_najkraci_put(start, end, graph):
+    posjeceni_brojevi = [start]
+    putevi = []
+
     for i in range(14):
-        if graf[pocetni_broj][i] != 0:
-            rekuzrzivna_duljina = pronadji_put(i, zavrsni_broj, graf, posjeceni_brojevi, graf[pocetni_broj][i])
-            if rekuzrzivna_duljina != -1:
-                duljina_puta.append(rekuzrzivna_duljina)
+        if graph[start][i] != 0:
+            put = pronadji_put(i, end, graph, posjeceni_brojevi, graph[start][i])
+            if put != -1:
+                putevi.append(put)
 
-    if len(duljina_puta) == 0:
+    if len(putevi) == 0:
         return -1
-    return min(duljina_puta)
+    else:
+        return min(putevi)
 
-def pronadji_put(trenutni_broj, zavrsni_broj, posjeceni_brojevi, graf, pozicija_na_grafu):
-    if trenutni_broj == zavrsni_broj:
+
+def pronadji_put(trenutni_broj, krajnji_broj, graf, posjeceni_brojevi, pozicija_na_grafu):
+    if trenutni_broj == krajnji_broj:
         return pozicija_na_grafu
 
-    rekurzivne_duljine = []
+    duljine = []
     posjeceni_brojevi.append(trenutni_broj)
     for i in range(14):
         if graf[trenutni_broj][i] != 0:
             if i not in posjeceni_brojevi:
-                rekuzrzivna_duljina = pronadji_put(i, zavrsni_broj, graf, posjeceni_brojevi, graf[trenutni_broj][i] + pozicija_na_grafu)
-                rekurzivne_duljine.append(rekuzrzivna_duljina)
-    if len(rekurzivne_duljine) == 0:
-        return -1
-    return min(rekurzivne_duljine)
+                put = pronadji_put(i, krajnji_broj, graf, posjeceni_brojevi, pozicija_na_grafu + graf[trenutni_broj][i])
+                if put != -1:
+                    duljine.append(put)
 
+    if len(duljine) == 0:
+        return -1
+    else:
+        return min(duljine)
 
 edgeovi = []
 for i in range(14):
@@ -51,9 +56,9 @@ for i in range(7):
     edgeovi[i % 7 + 7][(i+1) % 7 + 7] = 30
     edgeovi[(i+1) % 7 + 7][i % 7 + 7] = 30
 
-print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in edgeovi]))
+#print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in edgeovi]))
 
-print(pronadji_najkraci_put(pocetni_broj, zavrsni_broj, edgeovi))
+print(pronadji_najkraci_put(pocetni_broj-1, zavrsni_broj-1, edgeovi))
 # brojac_prvi = []
 # brojac_drugi = []
 # for i in range(len(edgeovi)):
