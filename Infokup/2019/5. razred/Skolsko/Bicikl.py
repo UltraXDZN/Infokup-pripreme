05c651903012a7bd34a87baca31761543cb592c9
UltraXDZN <ultraxdzn@gmail.com>
2021-01-30 23:36:57 +0100

    Rijesen zadatak Bicikl

---
+++ Infokup/2019/5. razred/Skolsko/Bicikl.py
@@ -0,0 +1,19 @@
+visina = int(input())
+
+if visina < 155:
+    print("XS")
+
+elif 156 < visina < 165:
+    print("S")
+
+elif 166 < visina < 175:
+    print("M")
+
+elif 176 < visina < 183:
+    print("L")
+
+elif 184 < visina < 192:
+    print("XL")
+
+else:
+    print("XXL")
