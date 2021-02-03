4b1ef95b557431ed0eadd66a8cbde110858e5bef
UltraXDZN <ultraxdzn@gmail.com>
2021-02-03 23:48:46 +0100

    Rijesen zadatak Jaja

---
+++ Infokup/2020/5. razred/Skolsko/Jaja.py
@@ -0,0 +1,12 @@
+broj = int(input())
+
+if broj >= 73:
+    print("A XL")
+if 73 > broj >= 63:
+    print("A L")
+if 63 > broj >= 53:
+    print("A M")
+if 53 > broj >= 43:
+    print("A S")
+if 43 > broj:
+    print("B XS")
