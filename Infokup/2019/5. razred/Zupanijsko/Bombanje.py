4a8c333b38de37886bb4755730d271ea28ba131a
UltraXDZN <ultraxdzn@gmail.com>
2021-02-08 00:10:38 +0100

    Rijesen zadatak Bombanje

--- Infokup/2019/5. razred/Zupanijsko/Bombanje.py
+++ Infokup/2019/5. razred/Zupanijsko/Bombanje.py
@@ -8,13 +8,13 @@ for i in range(broj_rundi_uk):
     igracevi_brojevi.append(int(input()))
     generatorovi_brojevi.append(int(input()))
 
-brojevi = dict(zip(igracevi_brojevi, generatorovi_brojevi))
+brojevi = [*zip(igracevi_brojevi, generatorovi_brojevi)]
 zbroj = 0
 increment = 0
 decrement = 1
 
-for k in brojevi:
-    if k == brojevi[k]:
+for i in range(len(brojevi)):
+    if brojevi[i][0] == brojevi[i][1]:
         zbroj += pocetni_broj_bombona + increment
         increment += 1
     else:
