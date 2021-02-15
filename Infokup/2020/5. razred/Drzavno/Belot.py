75907114fd0d132e5fbe12d4c56e7a6613853a77
UltraXDZN <ultraxdzn@gmail.com>
2021-02-15 23:58:04 +0100

    Rijesen zadatak Belot

--- Infokup/2020/5. razred/Drzavno/Belot.py
+++ Infokup/2020/5. razred/Drzavno/Belot.py
@@ -8,21 +8,24 @@ for i in range(8):
     ruka.append(value)
 
 rjesenje = []
+dvojke = []
 for i in reversed(range(8)):
     for j in reversed(range(8)):
-        for k in reversed(range(min(i, j), 8)):
+        for k in reversed(range(8)):
             if ruka[i][1] == ruka[j][1] == ruka[k][1]:
-                if vrijendosti.index(ruka[k][0]) > vrijendosti.index(ruka[j][0]) > vrijendosti.index(ruka[i][0]):
+                if vrijendosti.index(ruka[k][0]) - vrijendosti.index(ruka[j][0]) == 1 and vrijendosti.index(ruka[j][0]) - vrijendosti.index(ruka[i][0]) == 1:
                     rjesenje.append(["1", ruka[i][1], ruka[i][0]])
-            for l in reversed(range(8)):
-                if ruka[i][0] == ruka[j][0] == ruka[k][0] == ruka[l][0]:
-                    if ruka[i][1] != ruka[j][1] != ruka[k][1] != ruka[l][1]:
-                        rjesenje.append(["2", ruka[i][0]])
+                    break
 
+            for l in reversed(range(8)):
+                if len({ruka[i][0], ruka[j][0], ruka[k][0], ruka[l][0]}) <= 1:
+                    if len(list(set([ruka[i][1], ruka[j][1], ruka[k][1], ruka[l][1]]))) == 4 and sorted([ruka[i][1], ruka[j][1], ruka[k][1], ruka[l][1]]) not in dvojke:
+                        rjesenje.append(["2", ruka[i][0]])
+                        dvojke.append(sorted([ruka[i][1], ruka[j][1], ruka[k][1], ruka[l][1]]))
+                        break
 myFinallist = []
 for i in rjesenje:
     if i not in myFinallist:
         myFinallist.append(i)
         print(*i)
 
-# print(ruka)
