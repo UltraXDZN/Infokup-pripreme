4dad20e0159301029b6e2d768510b1b35b6f23b6
UltraXDZN <ultraxdzn@gmail.com>
2021-02-18 18:33:15 +0100

    Update Selotejpet (prvi dio radi)

--- Infokup/2020/5. razred/Drzavno/Selotejpet.py
+++ Infokup/2020/5. razred/Drzavno/Selotejpet.py
@@ -1,17 +1,27 @@
 broj_komada = int(input())
 podjela = int(input())
 
-daska_za_cvarke = [[]] * podjela
+daska = [""] * podjela
 
 komadi_limiti = []
 for i in range(broj_komada):
-    temp_unos = input().split()
+    temp_unos = [int(num) for num in input().split()]
     komadi_limiti.append(temp_unos)
 
 for i in range(len(komadi_limiti)):
-    for j in range(int(komadi_limiti[i][0]), int(komadi_limiti[i][1])+1):
-        print(j)
-        daska_za_cvarke[j].append(f"#{i+1}")
-    print("--------")
+    temp_komad = komadi_limiti[i]
+    temp_komad = [num for num in range(temp_komad[0]-1, temp_komad[-1])]
+    for num in temp_komad:
+        daska[num] += str(i+1)
 
-print(daska_za_cvarke)
+temp_len = []
+for komad in daska:
+    temp_len.append(len(komad))
+print(max(temp_len))
+
+# prvi_komad = 0
+# for komad in daska:
+#     if komad != "":
+#         print(komad[0])
+#         break
+
