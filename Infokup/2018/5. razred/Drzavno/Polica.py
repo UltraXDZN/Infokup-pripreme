9a65502eaec5d9fda4115bae0f9a722df5e384d2
UltraXDZN <ultraxdzn@gmail.com>
2021-03-13 23:22:54 +0100

    Zapocet zadatak Polica

--- Infokup/2018/5. razred/Drzavno/Polica.py
+++ Infokup/2018/5. razred/Drzavno/Polica.py
@@ -1,1 +1,16 @@
-odabrani_broj = int(input())
+broj_oznacenih_kamena = int(input())
+
+kamenje = {}
+
+for i in range(1, broj_oznacenih_kamena+1):
+    kamenje[i] = int(input())
+
+broj_preslagivanja = int(input())
+
+preslagivanja = []
+for i in range(broj_preslagivanja):
+    preslagivanja.append(int(input()))
+
+print(kamenje)
+print(preslagivanja)
+
