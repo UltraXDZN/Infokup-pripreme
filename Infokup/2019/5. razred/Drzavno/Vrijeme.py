2ca9c2613e645511abcf2e499b04566e19ebbb06
UltraXDZN <ultraxdzn@gmail.com>
2021-03-25 23:29:27 +0100

    Polu-rijesen Zadatak Vrijeme

--- Infokup/2019/5. razred/Drzavno/Vrijeme.py
+++ Infokup/2019/5. razred/Drzavno/Vrijeme.py
@@ -1,25 +1,22 @@
-zadano_vrijeme = input()
+zadano_vrijeme = "0" + input()
+if len(zadano_vrijeme) < 4:
+    zadano_vrijeme += "0"
+
+rastavljeno_vrijeme = list(zadano_vrijeme)
+print(rastavljeno_vrijeme)
 
 opcije_vremena = []
-if len(zadano_vrijeme) == 1:
-    opcije_vremena.append((0, int(zadano_vrijeme)))
-    opcije_vremena.append((int(zadano_vrijeme), 0))
-elif len(zadano_vrijeme) == 2:
-    opcije_vremena.append((0, int(zadano_vrijeme)))
-    opcije_vremena.append((int(zadano_vrijeme[0]), int(zadano_vrijeme[1])))
-    opcije_vremena.append((int(zadano_vrijeme), 0))
 
-else:
-    sati, minute = 0, 0
-    sati = int(zadano_vrijeme[0:1]) if int(zadano_vrijeme[0:2]) > 23 else int(zadano_vrijeme[0:2])
-    minute = int(zadano_vrijeme.replace(str(sati), ""))
-    if 0 > minute > 59:
-        opcije_vremena(None)
-    opcije_vremena.append((sati, minute))
+for i in range(1, len(zadano_vrijeme)+1):
+    if int(zadano_vrijeme[:i]) <= 23 and int(zadano_vrijeme[i:]) <= 59:
+        if "0" not in zadano_vrijeme[-2::-i]:
+            opcije_vremena.append((zadano_vrijeme[1:i], zadano_vrijeme[i:]))
 
-for opcija in opcije_vremena:
-    if opcija is not None:
-        print(f"{opcija[0]}:{opcija[1]}")
-    else:
-        print("TLE")
+print(opcije_vremena)
+
+# for opcija in opcije_vremena:
+#     if opcija is not None:
+#         print(f"{opcija[0]}:{opcija[1]}")
+#     else:
+#         print("TLE")
 
