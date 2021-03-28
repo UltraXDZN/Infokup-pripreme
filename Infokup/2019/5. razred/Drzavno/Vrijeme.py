087b83a8cd1a8b44713271331bb5c97f99bd6f0b
UltraXDZN <ultraxdzn@gmail.com>
2021-03-28 23:33:57 +0200

    Polu-rijesen Zadatak Vrijeme

--- Infokup/2019/5. razred/Drzavno/Vrijeme.py
+++ Infokup/2019/5. razred/Drzavno/Vrijeme.py
@@ -2,21 +2,18 @@ zadano_vrijeme = "0" + input()
 if len(zadano_vrijeme) < 4:
     zadano_vrijeme += "0"
 
-rastavljeno_vrijeme = list(zadano_vrijeme)
-print(rastavljeno_vrijeme)
-
 opcije_vremena = []
 
 for i in range(1, len(zadano_vrijeme)+1):
-    if int(zadano_vrijeme[:i]) <= 23 and int(zadano_vrijeme[i:]) <= 59:
+    if int(zadano_vrijeme[:i]) <= 23 and int(zadano_vrijeme[i:i+2]) <= 59:
         if "0" not in zadano_vrijeme[-2::-i]:
-            opcije_vremena.append((zadano_vrijeme[1:i], zadano_vrijeme[i:]))
+            opcije_vremena.append((zadano_vrijeme[1:i], zadano_vrijeme[i:i+2]))
 
 print(opcije_vremena)
 
-# for opcija in opcije_vremena:
-#     if opcija is not None:
-#         print(f"{opcija[0]}:{opcija[1]}")
-#     else:
-#         print("TLE")
+for opcija in opcije_vremena:
+    if opcija is not None:
+        print(f"{opcija[0]}:{opcija[1]}")
+    else:
+        print("TLE")
 
