f844c1e38f01f546456161c248675853f7615c91
UltraXDZN <ultraxdzn@gmail.com>
2021-04-14 23:42:59 +0200

    Unos radi - zadatak Bojan

--- Infokup/2017/5. razred/Drzavno/Bojan.py
+++ Infokup/2017/5. razred/Drzavno/Bojan.py
@@ -1,2 +1,6 @@
-unos = int(input())
-#Update2
+broj_stupaca = int(input())
+stupci = [int(i) for i in input().split()]
+broj_bojanja_zida = int(input())
+koraci = []
+for i in range(broj_bojanja_zida):
+    koraci.append([int(i) for i in input().split()])
