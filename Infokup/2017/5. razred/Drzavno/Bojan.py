79f7bab6c0116a30e3a966f6a539a8edf1f83439
UltraXDZN <ultraxdzn@gmail.com>
2021-04-15 20:18:19 +0200

    Selekcija radi - zadatak Bojan

--- Infokup/2017/5. razred/Drzavno/Bojan.py
+++ Infokup/2017/5. razred/Drzavno/Bojan.py
@@ -1,6 +1,13 @@
+from typing import List
+
 broj_stupaca = int(input())
 stupci = [int(i) for i in input().split()]
 broj_bojanja_zida = int(input())
 koraci = []
 for i in range(broj_bojanja_zida):
     koraci.append([int(i) for i in input().split()])
+
+for korak in koraci:
+    print()
+    for i in range(korak[0]-1, korak[1]):
+        print(stupci[i], end=" ")
