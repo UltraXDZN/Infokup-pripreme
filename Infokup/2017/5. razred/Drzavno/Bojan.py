d9bcdcdcde2fb7dd3cffd01c5e758ff3fec8fc9f
UltraXDZN <ultraxdzn@gmail.com>
2021-04-16 20:42:42 +0200

    Dio radi - zadatak Bojan

--- Infokup/2017/5. razred/Drzavno/Bojan.py
+++ Infokup/2017/5. razred/Drzavno/Bojan.py
@@ -9,33 +9,25 @@ for i in range(broj_bojanja_zida):
 # print(stupci)
 boja_na_zidu = [0] * broj_stupaca
 for korak in koraci:
-    print(f"---------------------------------------------------------- {koraci.index(korak)} ")
+    # print(f"---------------------------------------------------------- {koraci.index(korak)} ")
     trenutna_boja = korak[2]
-    trenutna_lokacija = korak[0] - 1
+    trenutna_lokacija = korak[0]-1
     promijeni_smjer = False
     while trenutna_boja != 0:
-
-        if stupci[trenutna_lokacija - 1] == 0:
-            trenutna_lokacija += 1
-            continue
-
-        print(f"--------------------------------------- {trenutna_boja}, {trenutna_lokacija}, {promijeni_smjer} pocetak")
-        if trenutna_lokacija >= korak[1] or trenutna_lokacija < korak[0] - 1:
+        # print(f"--------------------------------------- {trenutna_boja}, {trenutna_lokacija}, {promijeni_smjer} pocetak")
+        if trenutna_lokacija >= korak[1] or trenutna_lokacija < korak[0]-1:
             promijeni_smjer = not promijeni_smjer
-            boja_na_zidu[trenutna_lokacija] += 1
             if promijeni_smjer:
                 trenutna_lokacija -= 1
             else:
                 trenutna_lokacija += 1
         trenutna_boja -= 1
         boja_na_zidu[trenutna_lokacija] += 1
-        # trenutna_lokacija += 1
+        #trenutna_lokacija += 1
 
         if promijeni_smjer:
             trenutna_lokacija -= 1
         else:
             trenutna_lokacija += 1
-        print(f"--------------------------------------- {trenutna_boja}, {trenutna_lokacija}, {promijeni_smjer} kraj")
-        print(boja_na_zidu)
-
+        # print(f"--------------------------------------- {trenutna_boja}, {trenutna_lokacija}, {promijeni_smjer} kraj")
 print(*boja_na_zidu)
