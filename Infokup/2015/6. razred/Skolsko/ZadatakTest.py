3224aa708c92ecfc876df47724c087ce9a5c8e78
UltraXDZN <ultraxdzn@gmail.com>
2021-05-01 23:56:14 +0200

    Zadatak Test - Polovicno rjesen

---
+++ Infokup/2015/6. razred/Skolsko/ZadatakTest.py
@@ -0,0 +1,27 @@
+broj_ucenika = int(input())
+
+parni_ucenici = []
+neparni_ucenici = []
+
+for i in range(1, broj_ucenika+1):
+    ocjena = int(input())
+    if ocjena % 2 == 0:
+        parni_ucenici.append(ocjena)
+    else:
+        neparni_ucenici.append(ocjena)
+
+prosjek_parnih = sum(parni_ucenici) / len(parni_ucenici)
+prosjek_neparnih = sum(neparni_ucenici) / len(neparni_ucenici)
+
+
+rjesenje = ""
+if prosjek_neparnih > prosjek_parnih:
+    rjesenje = "N"
+elif prosjek_parnih > prosjek_neparnih:
+    rjesenje = "P"
+else:
+    rjesenje = "NP"
+
+print(f"{prosjek_neparnih:.2f}")
+print(f"{prosjek_parnih:.2f}")
+print(rjesenje)
