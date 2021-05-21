9b0de329b67f42b1f92b60912ea314db1fd71f83
UltraXDZN <ultraxdzn@gmail.com>
2021-05-21 23:57:00 +0200

    Zadatak Puni

---
+++ Infokup/2015/6. razred/Zupanijsko/Puni.py
@@ -0,0 +1,9 @@
+datumi = [int(input()), int(input()), int(input()), int(input())]
+tren_datum = int(input())
+
+stanje_mjeseca = {
+    "MLADI": datumi[0],
+    "PRVA": datumi[1],
+    "PUNI": datumi[2],
+    "ZADNJA": datumi[3]
+}
