c4324a5133e903c1e2560af65af7343c0ca874da
UltraXDZN <ultraxdzn@gmail.com>
2021-05-23 22:55:57 +0200

    Zadatak Puni - pola rješen

--- Infokup/2015/6. razred/Zupanijsko/Puni.py
+++ Infokup/2015/6. razred/Zupanijsko/Puni.py
@@ -8,9 +8,22 @@ stanje_mjeseca = {
     "ZADNJA": datumi[3]
 }
 
-for k in stanje_mjeseca.keys():
-    for k2 in stanje_mjeseca.keys():
-        if stanje_mjeseca[k] < tren_datum <= stanje_mjeseca[k2]:
-            print(k)
-        elif stanje_mjeseca[k] >= tren_datum > stanje_mjeseca[k2]:
-            print(k2)
+lista_vrijednosti = []
+
+for v in stanje_mjeseca.values():
+    lista_vrijednosti.append(v)
+
+for i in range(len(lista_vrijednosti)):
+    if tren_datum < lista_vrijednosti[i]:
+        print(list(stanje_mjeseca.keys())[i - 1])
+        break
+    elif tren_datum == lista_vrijednosti[i]:
+        print(list(stanje_mjeseca.keys())[i])
+        break
+
+# for k in stanje_mjeseca.keys():
+#     for k2 in stanje_mjeseca.keys():
+#         if stanje_mjeseca[k] < tren_datum <= stanje_mjeseca[k2]:
+#             print(k)
+#         elif stanje_mjeseca[k] >= tren_datum > stanje_mjeseca[k2]:
+#             print(k2)
