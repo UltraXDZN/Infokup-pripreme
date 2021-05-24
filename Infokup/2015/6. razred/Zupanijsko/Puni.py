2e05728250ef6c9071549f85384681952e73e4d0
UltraXDZN <ultraxdzn@gmail.com>
2021-05-24 22:50:21 +0200

    Zadatak Puni - riješen

--- Infokup/2015/6. razred/Zupanijsko/Puni.py
+++ Infokup/2015/6. razred/Zupanijsko/Puni.py
@@ -8,22 +8,20 @@ stanje_mjeseca = {
     "ZADNJA": datumi[3]
 }
 
-lista_vrijednosti = []
+datumi_stanja = list(stanje_mjeseca.values())
+nazivi_stanja = list(stanje_mjeseca.keys())
 
-for v in stanje_mjeseca.values():
-    lista_vrijednosti.append(v)
-
-for i in range(len(lista_vrijednosti)):
-    if tren_datum < lista_vrijednosti[i]:
-        print(list(stanje_mjeseca.keys())[i - 1])
-        break
-    elif tren_datum == lista_vrijednosti[i]:
-        print(list(stanje_mjeseca.keys())[i])
+for i in range(len(datumi_stanja)):
+    if tren_datum < datumi_stanja[i]:
+        print(nazivi_stanja[i - 1])
+        print(datumi_stanja[i] - tren_datum)
         break
 
-# for k in stanje_mjeseca.keys():
-#     for k2 in stanje_mjeseca.keys():
-#         if stanje_mjeseca[k] < tren_datum <= stanje_mjeseca[k2]:
-#             print(k)
-#         elif stanje_mjeseca[k] >= tren_datum > stanje_mjeseca[k2]:
-#             print(k2)
+    elif tren_datum == datumi_stanja[i]:
+        print(nazivi_stanja[i])
+        if tren_datum >= datumi_stanja[-1]:
+            print(31 - tren_datum)
+            break
+        print(datumi_stanja[i + 1] - tren_datum)
+        break
+
