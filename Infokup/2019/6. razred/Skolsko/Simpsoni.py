3d49be2dc536b83c070020e25275b0ce462868af
UltraXDZN <ultraxdzn@gmail.com>
2021-07-08 23:51:16 +0200

    Rješen zadatak Simpsoni (1/2)

--- Infokup/2019/6. razred/Skolsko/Simpsoni.py
+++ Infokup/2019/6. razred/Skolsko/Simpsoni.py
@@ -1,12 +1,28 @@
 prednost = int(input())
 broj_bacanja = int(input())
 
-bodovi = []
+
+
+bacanja = []
+konos_bodovi = 0
+kang_bodovi = 0
+
+konos_runde = 0
+kang_runde = 0
 
 for i in range(broj_bacanja):
-    bodovi.append([int(broj) for broj in input().split()])
+    bacanje = [int(broj) for broj in input().split()]
+    bacanja.append(bacanje)
+    if bacanje[0] > bacanje[1]:
+        konos_bodovi += sum(bacanje)
+        konos_runde += 1
+    elif bacanje[1] > bacanje[0]:
+        kang_bodovi += sum(bacanje)
+        kang_runde += 1
 
 if prednost % 2 == 0:
     print("KANG")
 else:
     print("KONOS")
+print(konos_runde, kang_runde)
+print(konos_bodovi, kang_bodovi)
