7c4f6cb02dbe2257331ec78f3aa98e20eb8c2c8a
UltraXDZN <ultraxdzn@gmail.com>
2021-11-19 18:05:10 +0100

    Codeforces update

--- CodeForces/QueueAtTheSchool.py
+++ CodeForces/QueueAtTheSchool.py
@@ -1,14 +1,7 @@
 n, t = map(int, input().split())
-queue = list(input())
+queue = input()
 
-if queue[-1] == "G":
-    change = []
-    for i in range(1, t + 1):
-        if queue[i:].count("G") < queue[i:].count("B"):
-            change.append(queue[i])
-            queue.pop(0)
-        else:
-            change.append(queue[-i])
-            queue.pop(-1)
-    print(change)
-print("".join(change) + "".join(queue))
+for i in range(t):
+    queue = queue.replace("BG", "GB")
+
+print(queue)
