0e74bd1732f8e0857d1090dbbcbb46e5cd329de5
UltraXDZN <ultraxdzn@gmail.com>
2021-12-30 23:43:17 +0100

    Codeforces Update

--- CodeForces/TheBrandNewFunction.py
+++ CodeForces/TheBrandNewFunction.py
@@ -3,11 +3,10 @@ a = [int(x) for x in input().split()]
 
 ans = set()
 cur_ans = set()
-for i in range(n+1):
-    for j in range(i, len(cur_ans)):
-        cur_ans.add(a[i] | a[j])
+for i in a:
+    cur_ans = {i | j for j in cur_ans}
     cur_ans.add(i)
     ans.update(cur_ans)
     # print(cur_ans, ans, "----------", sep="\n") # DEBUG
-
 print(len(ans))
+
